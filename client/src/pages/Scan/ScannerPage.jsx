afcffbaf6a0e4bb99cfb408f7654acbe95f4918b
Alfredo Limin <jjlimin@yahoo.com>
2025-10-25 12:23:27 +0300

    scanner detection debug

--- client/src/pages/Scan/ScannerPage.jsx
+++ client/src/pages/Scan/ScannerPage.jsx
@@ -14,27 +14,28 @@ function ScannerPage() {
             try {
                 const res = await scanTicket(data.text);
                 setMessage(`${res.data.message}`);
+                alert("success");
             } catch (error) {
                 setMessage("Invalid");
+                alert("invalid");
             }
         }
     }
   return (
-    <div className="flex felx-col items-center mt-10">
+    <div className="flex flex-col items-center mt-10">
         <h1 className="text-xl font-bold mb-4">Ticket Scanner</h1>
         <div className="w-80 border rounded-xl overflow-hidden">
             <QrReader
-                constraints={{facingMode: "environment"}}
+                constraints={{facingMode: {ideal: "environment"} }}
+                scanDelay={300}
                 onResult={(data, error) => {
-                    if (data) {
-                        console.log("Scanned:", data.text);
-                        handleScan(data);
-                    }
+                    if (data) handleScan(data);
                     if (error) console.info(error);
                 }}
+                style={{width: "100%"}}
             />
-            <p className="mt-4 text-gray-700">{message}</p>
         </div>
+        <p className="mt-4 text-gray-700">{message}</p>
     </div>
   )
 }
